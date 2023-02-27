6df2e996b6a85ba20b1e1e47ec0068e5742d4ecc
3li07 <mahery.bemanantsoa@esti.mg>
2023-02-27 19:11:00 +0300

    make migration sprint 1💾

---
+++ Backend/app/Models/GroupePermission.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GroupePermission extends Model
+{
+    use HasFactory;
+    protected $table = 'groupe_permission';
+}
